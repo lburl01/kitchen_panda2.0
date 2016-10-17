ed0967ee959d205a6e9376bb835f1a781c18c71d
Lori Burleson <lori.burleson@gmail.com>
2016-10-16 21:42:01 -0400

    All locations can now filter appropriately and can have items deleted

--- public/app.js
+++ public/app.js
@@ -50,9 +50,63 @@ $(function() {
     url: '/items',
     dataType: 'json'
   }).done(function(items) {
-    var $items = $('#items')
+    var $items = $('#items-pantry')
     items.forEach(function(item) {
-      if (item.is_deleted == false ) {
+      if (item.is_deleted == false && item.location_id == 3 ) {
+        var $p = $('<p>').appendTo($items)
+        var $span = $('<span>').text(item.name + ": " + item.quantity).appendTo($p)
+        var $edit = $('<button>').text("Edit").addClass('btn btn-default crud').appendTo($p)
+        var $delete = $('<button>').text(" X ").addClass('btn btn-default delete crud').appendTo($p)
+
+        $delete.click(function() {
+          $p.remove();
+
+          $.ajax({
+            method: 'PUT',
+            url: '/items/remove/' + item.id
+          })
+        })
+      }
+      })
+    })
+})
+
+$(function() {
+  $.ajax({
+    method: 'GET',
+    url: '/items',
+    dataType: 'json'
+  }).done(function(items) {
+    var $items = $('#items-freezer')
+    items.forEach(function(item) {
+      if (item.is_deleted == false && item.location_id == 1 ) {
+        var $p = $('<p>').appendTo($items)
+        var $span = $('<span>').text(item.name + ": " + item.quantity).appendTo($p)
+        var $edit = $('<button>').text("Edit").addClass('btn btn-default crud').appendTo($p)
+        var $delete = $('<button>').text(" X ").addClass('btn btn-default delete crud').appendTo($p)
+
+        $delete.click(function() {
+          $p.remove();
+
+          $.ajax({
+            method: 'PUT',
+            url: '/items/remove/' + item.id
+          })
+        })
+      }
+      })
+    })
+})
+
+$(function() {
+  $.ajax({
+    method: 'GET',
+    url: '/items',
+    dataType: 'json'
+  }).done(function(items) {
+    var $items = $('#items-fridge')
+    items.forEach(function(item) {
+      if (item.is_deleted == false && item.location_id == 2 ) {
         var $p = $('<p>').appendTo($items)
         var $span = $('<span>').text(item.name + ": " + item.quantity).appendTo($p)
         var $edit = $('<button>').text("Edit").addClass('btn btn-default crud').appendTo($p)
